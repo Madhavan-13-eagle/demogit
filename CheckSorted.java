b45667446b99b145efb0db7c6ddfd6900eaa0508
Madhavan1302 <madhavan0823@gmail.com>
2025-03-18 21:02:39 +0530

    two new Files added

---
+++ CheckSorted.java
@@ -0,0 +1,22 @@
+public class CheckSorted{
+    public static void main(String []args){
+        int arr[]={1,2,3,4,5,6,7};
+        System.out.println(isSorted(arr,0));
+        System.out.println(search(arr,9,0));
+    }
+    public static boolean isSorted(int []arr,int idx){
+        if(idx==arr.length-1){
+            return true;
+        }
+        return (arr[idx]<arr[idx+1])&&isSorted(arr,idx+1); 
+    }
+    public static int search(int[]arr,int target,int idx){
+        if(arr[idx]==target){
+            return idx;
+        }
+        if(arr.length-1==idx){
+            return -1;
+        }
+        return search(arr,target,idx+1);
+    }
+}
---
+++ LongestSubarrayWith0and1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LongestSubarrayWith0and1{
+    public static void getSubArray(int []arr){
+        int[][]arrl=new int[100][];
+        int ind1=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                
+            }
+        }
+        for(int i=0;i<arrl.length;i++){
+            System.out.println(Arrays.toString(arrl[i]));
+        };
+    }
+    public static void main(String []aras){
+        getSubArray(new int[]{1,0,1,1,1,0,0});
+    }
+}
