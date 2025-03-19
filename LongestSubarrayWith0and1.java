fc5983ee63f05ea34f7b654477457cdf0d66ca3c
Madhavan-13-eagle <142667544+Madhavan-13-eagle@users.noreply.github.com>
2025-03-19 14:15:48 +0530

    Update LongestSubarrayWith0and1.java

--- LongestSubarrayWith0and1.java
+++ LongestSubarrayWith0and1.java
@@ -3,11 +3,11 @@ public class LongestSubarrayWith0and1{
     public static void getSubArray(int []arr){
         int[][]arrl=new int[100][];
         int ind1=0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=i;j<arr.length;j++){
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=i;j<arr.length;j++){
                 
-            }
-        }
+        //     }
+        // }
         for(int i=0;i<arrl.length;i++){
             System.out.println(Arrays.toString(arrl[i]));
         };
