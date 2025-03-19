036bfad837458d48fb35643027c46c8626131c49
Madhavan1302 <madhavan0823@gmail.com>
2025-03-19 14:17:26 +0530

    Check sorted is changed

--- CheckSorted.java
+++ CheckSorted.java
@@ -1,8 +1,8 @@
 public class CheckSorted{
     public static void main(String []args){
         int arr[]={1,2,3,4,5,6,7};
-        System.out.println(isSorted(arr,0));
-        System.out.println(search(arr,9,0));
+        // System.out.println(isSorted(arr,0));
+        // System.out.println(search(arr,9,0));
     }
     public static boolean isSorted(int []arr,int idx){
         if(idx==arr.length-1){
